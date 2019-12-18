e84841096f82b8c1f5b9580b652f9c7b2ee4faec
EC2 Default User <ec2-user@ip-172-31-32-32.ap-northeast-1.compute.internal>
2019-12-18 12:15:58 +0000

    #11 link_toヘルパーを使ってみよう

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all.order(created_at: 'desc')
-
     end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
 end
